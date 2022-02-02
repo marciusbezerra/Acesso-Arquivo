153f4b8e16028a116f0816430be09973330a6ebd
Marcius Bezerra <marciusbezerra@outlook.com>
2022-02-02 10:52:19 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Acesso-Arquivo
+
+_Autor:\
+Marcius Bezerra]
+[marciusbezerr@gmail.com](mailto:marciusbezerr@gmail.com)_
+
+![Acesso-Arquivo](readme.jpeg)
+
+Exemplo simples de como persistir dados em arquivo usando o Flutter.
---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+// Flutter code sample for material.AppBar.1
+
+// This sample shows an [AppBar] with two simple actions. The first action
+// opens a [SnackBar], while the second action navigates to a new page.
+
+//import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
+void main() => runApp(
+    MaterialApp(
+      title: 'Ler Arquivos',
+      home: MyApp(storage: CounterStorage()),
+    ),
+  );
+
+/// This Widget is the main application widget.
+class MyApp extends StatefulWidget {
+  final CounterStorage storage;
+
+  MyApp({Key key, @required this.storage}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+   int _counter;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.storage.readCounter().then((int value) {
+      setState(() {
+        _counter = value;
+      });
+    });
+  }
+
+  Future<File> _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+
+    // Write the variable as a string to the file.
+    return widget.storage.writeCounter(_counter);
+  }
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Reading and Writing Files')),
+      body: Center(
+        child: Text(
+          'Botão clicado $_counter vez${_counter == 1 ? '' : 'es'}.',
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class CounterStorage {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/counter.txt');
+  }
+
+  Future<int> readCounter() async {
+    try {
+      final file = await _localFile;
+
+      // Read the file
+      String contents = await file.readAsString();
+
+      return int.parse(contents);
+    } catch (e) {
+      // If encountering an error, return 0
+      return 0;
+    }
+  }
+
+  Future<File> writeCounter(int counter) async {
+    final file = await _localFile;
+
+    // Write the file
+    return file.writeAsString('$counter');
+  }
+}
